8f3ac622d0623c8896138172d773c9f0c2b987f0
Saad Azeem <saadazeem626@gmail.com>
2026-09-22 17:36:54 +0500

    Fixed the Local vs. Production Authentication Difference issue by Updating datapilot-api to datapilot-backend:v4

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,7 +18,9 @@ const isProtectedRoute = createRouteMatcher([
 
 const clerkHandler = clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    await auth.protect();
+    await auth.protect({
+      unauthenticatedUrl: new URL("/sign-in", req.url).toString(),
+    });
   }
 });
 
